79ae827b06f3442f36fe3b5cd1770c27c691069e
Will Malisch <malisch.will@gmail.com>
2020-05-20 13:16:19 -0400

    Set up todo model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+# Allows you to read a field that is not allowed to be edited
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+admin.site.register(Todo, TodoAdmin)
