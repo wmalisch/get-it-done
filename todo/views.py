ff9905968f66e920739732c1efc704ba523cf897
Will Malisch <malisch.will@gmail.com>
2020-05-20 14:54:50 -0400

    List todo based on user and importance

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -24,7 +25,9 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html',{'form':UserCreationForm(), 'error':'Passwords did not match'})
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html',{'todos':todos})
+
 
 
 def logoutuser(request):
